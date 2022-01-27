c7f14d770a83a5817d8f20d476f558e3227249d1
Andrei Mandychev <dev@dappli.fr>
2022-01-27 23:29:36 +0100

    [Server] implement file route with encoded fileId

---
+++ server/src/jvmMain/kotlin/Routes.kt
@@ -0,0 +1,66 @@
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import fr.dappli.photocloud.vo.Config
+import fr.dappli.photocloud.vo.Dir
+import fr.dappli.photocloud.vo.User
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.io.File
+import java.util.*
+
+fun Route.handleHomeRequests() {
+    get("/") {
+        call.respondText("Hello, Server!")
+    }
+}
+
+fun Route.handleLoginRequests(
+    secret: String,
+    issuer: String,
+    audience: String,
+    confUser: User
+) {
+    post("/login") {
+        val user = call.receive<User>()
+        // Check username and password
+        if (user == confUser) {
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("username", user.username)
+                .withExpiresAt(Date(System.currentTimeMillis() + 30 * 60000))
+                .sign(Algorithm.HMAC256(secret))
+            call.respond(hashMapOf("token" to token))
+        } else {
+            call.response.status(HttpStatusCode.Forbidden)
+        }
+    }
+}
+
+fun Route.handleConfigRequests() {
+    val root = SCHEME
+    val fileId = Base64.getUrlEncoder().withoutPadding().encodeToString(root.toByteArray())
+    val config = Config(Dir(fileId))
+
+    get("/config") {
+        call.respond(config)
+    }
+}
+
+fun Route.handleFileRequests(rootPath: String) {
+    get("/file/{fileId}") {
+        val fileId = call.parameters["fileId"] ?: throw BadRequestException("fileId is null")
+        val fileUrl = String(Base64.getUrlDecoder().decode(fileId))
+        val path = pathRegex.matchEntire(fileUrl)?.groups?.get(1)?.value ?: throw BadRequestException("unrecognized")
+        val files = File("$rootPath/$path").listFiles()
+        val fileNames = files?.map { it.name } ?: emptyList()
+        call.respondText("$path>: $fileNames")
+    }
+}
+
+private const val SCHEME ="photocloud://"
+private val pathRegex = "photocloud:\\/\\/(.*)".toRegex()
--- server/src/jvmMain/kotlin/Server.kt
+++ server/src/jvmMain/kotlin/Server.kt
@@ -1,37 +1,18 @@
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import fr.dappli.photocloud.vo.Config
-import fr.dappli.photocloud.vo.Dir
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.request.*
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
-import java.util.*
 import fr.dappli.photocloud.vo.User
-import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
     routing {
-        install(ContentNegotiation) {
-            json()
-        }
-        install(CORS) {
-            method(HttpMethod.Get)
-            method(HttpMethod.Post)
-            method(HttpMethod.Delete)
-            anyHost()
-        }
-        install(Compression) {
-            gzip()
-        }
-
         val secret = environment.config.property("jwt.secret").getString()
         val issuer = environment.config.property("jwt.issuer").getString()
         val audience = environment.config.property("jwt.audience").getString()
@@ -42,59 +23,56 @@ fun Application.module(testing: Boolean = false) {
         val confUser = User(confUserName, confPassword)
 
         val filePath = environment.config.property("server.filePath").getString()
-        val fileId = UUID.nameUUIDFromBytes(filePath.toByteArray()).toString()
-        val config = Config(Dir(fileId))
 
-        install(Authentication) {
-            jwt("auth-jwt") {
-                realm = myRealm
-                verifier(
-                    JWT.require(Algorithm.HMAC256(secret))
-                        .withAudience(audience)
-                        .withIssuer(issuer)
-                        .build()
-                )
-                validate { credential ->
-                    if (credential.payload.getClaim("username").asString() != "") {
-                        JWTPrincipal(credential.payload)
-                    } else {
-                        null
-                    }
+        setupServer()
+        setupAuthentication(secret, issuer, audience, myRealm)
+        handleLoginRequests(secret, issuer, audience, confUser)
+        handleHomeRequests()
+
+        authenticate("auth-jwt") {
+            handleConfigRequests()
+            handleFileRequests(filePath)
+        }
+    }
+}
+
+private fun Application.setupServer() {
+    install(ContentNegotiation) {
+        json()
+    }
+    install(CORS) {
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Delete)
+        anyHost()
+    }
+    install(Compression) {
+        gzip()
+    }
+}
+
+private fun Application.setupAuthentication(
+    secret: String,
+    issuer: String,
+    audience: String,
+    myRealm: String
+) {
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(
+                JWT.require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
                 }
             }
         }
-
-        post("/login") {
-            val user = call.receive<User>()
-            // Check username and password
-            if (user == confUser) {
-                val token = JWT.create()
-                    .withAudience(audience)
-                    .withIssuer(issuer)
-                    .withClaim("username", user.username)
-                    .withExpiresAt(Date(System.currentTimeMillis() + 30 * 60000))
-                    .sign(Algorithm.HMAC256(secret))
-                call.respond(hashMapOf("token" to token))
-            } else {
-                call.response.status(HttpStatusCode.Forbidden)
-            }
-        }
-
-        authenticate("auth-jwt") {
-            get("/config") {
-                call.respond(config)
-            }
-
-            get("files") {
-                val files = File(filePath).listFiles()
-                val fileNames = files?.map { it.name } ?: emptyList()
-                call.respondText("List of files in $filePath: $fileNames")
-            }
-        }
-
-        get("/") {
-            call.respondText("Hello, Server!")
-        }
     }
-
 }
