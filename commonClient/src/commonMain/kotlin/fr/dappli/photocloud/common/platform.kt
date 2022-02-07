ef0a815563e162f992abe6c67be68e1da109d371
Andrei Mandychev <dev@dappli.fr>
2022-02-07 23:03:25 +0100

    [Client][Desktop] create image bitmaps from the network

--- commonClient/src/commonMain/kotlin/fr/dappli/photocloud/common/platform.kt
+++ commonClient/src/commonMain/kotlin/fr/dappli/photocloud/common/platform.kt
@@ -1,5 +1,10 @@
 package fr.dappli.photocloud.common
 
+import androidx.compose.ui.graphics.ImageBitmap
+import java.io.InputStream
+
 expect fun getPlatformName(): String
 
 expect fun getDebugHost(): String
+
+expect fun loadBitmap(inputStream: InputStream): ImageBitmap
