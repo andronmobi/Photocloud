d9d5d275d6844d05a9d97778dda5b9ae1e1373aa
Andrei Mandychev <dev@dappli.fr>
2022-02-07 23:21:30 +0100

    [Android] decode bitmap from input stream

--- commonClient/src/androidMain/kotlin/fr/dappli/photocloud/common/platform.kt
+++ commonClient/src/androidMain/kotlin/fr/dappli/photocloud/common/platform.kt
@@ -1,9 +1,20 @@
 package fr.dappli.photocloud.common
 
+import android.graphics.BitmapFactory
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import java.io.BufferedInputStream
+import java.io.InputStream
+
 actual fun getPlatformName(): String {
     return "Android"
 }
 
 actual fun getDebugHost(): String {
     return "192.168.1.2"
+}
+
+actual fun loadBitmap(inputStream: InputStream): ImageBitmap {
+    val bitmap = BitmapFactory.decodeStream(BufferedInputStream(inputStream))
+    return bitmap.asImageBitmap()
 }
