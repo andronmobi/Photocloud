e060a2a97c7a28c68c405ad03e95ab02b233b595
Andrei Mandychev <dev@dappli.fr>
2022-03-14 23:43:03 +0100

    [Client] add sharedClient module

---
+++ sharedClient/src/commonMain/kotlin/fr/dappli/sharedclient/Greeting.kt
@@ -0,0 +1,7 @@
+package fr.dappli.sharedclient
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ sharedClient/src/commonMain/kotlin/fr/dappli/sharedclient/Platform.kt
@@ -0,0 +1,5 @@
+package fr.dappli.sharedclient
+
+expect class Platform() {
+    val platform: String
+}
