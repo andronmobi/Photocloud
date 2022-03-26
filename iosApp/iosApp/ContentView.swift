b2492847490940aadab1fb8f26c63e8c1212aaf7
Andrei Mandychev <dev@dappli.fr>
2022-03-26 23:15:03 +0100

    [Client][iOS] show a dummy image

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,10 +1,26 @@
 import SwiftUI
 import sharedClient
 
+extension String {
+    func load() -> UIImage {
+        do {
+            guard let url = URL(string: self) else {
+                return UIImage()
+            }
+            let data: Data = try Data(contentsOf: url)
+            return UIImage(data: data) ?? UIImage()
+        } catch {}
+        return UIImage()
+    }
+}
+
 struct ContentView: View {
 	let greet = Greeting().greeting()
 
 	var body: some View {
+        Image(uiImage: "https://www.pngall.com/wp-content/uploads/1/Android.png".load())
+            .resizable()
+            .frame(width: 100.0, height: 100.0)
 		Text(greet)
 	}
 }
