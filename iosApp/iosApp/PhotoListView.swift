72d348d4b85e000da817cc611077816c2507d2c1
Andrei Mandychev <dev@dappli.fr>
2022-05-01 18:02:35 +0200

    [ios] use LazyVGrid for photos

--- iosApp/iosApp/PhotoListView.swift
+++ iosApp/iosApp/PhotoListView.swift
@@ -10,50 +10,73 @@ struct PhotoListView: View {
 
     init(_ photoList: PhotoList) {
         self.photoList = photoList
-        self.model = ObservableValue(photoList.models)
+        model = ObservableValue(photoList.models)
     }
 
     var body: some View {
         if (photoList.isInitial) {
-            photosView()
+            foldersAndPhotos()
         } else {
             NavigationView {
-                VStack {
-                    photosView()
+                VStack(alignment: .leading) {
+                    foldersAndPhotos()
                 }
-                .navigationBarTitle(Text("Current dir name"), displayMode: .inline)
-                .navigationBarItems(
-                    leading: Image(systemName: "arrow.backward")
-                        .aspectRatio(contentMode: .fit)
-                        .imageScale(.large)
-                        .foregroundColor(.blue)
-                        .onTapGesture {
-                            photoList.onBackClicked()
-                        }
-                )
+                    .navigationBarTitle(Text("Current dir name"), displayMode: .inline)
+                    .navigationBarItems(
+                        leading: Image(systemName: "arrow.backward")
+                            .aspectRatio(contentMode: .fit)
+                            .imageScale(.large)
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                photoList.onBackClicked()
+                            }
+                    )
+            }
+        }
+    }
+
+    func foldersAndPhotos() -> some View {
+        VStack(alignment: .leading, spacing: 20) {
+            folders()
+            photosView()
+        }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .topLeading
+            ).padding([.horizontal], 8)
+    }
+
+    func folders() -> some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 16) {
+                ForEach(model.value.dirs, id: \.self.id) { photoDir in
+                    Button(action: {
+                        photoList.onDirClicked(dirId: photoDir.id)
+                    }, label: {
+                        Image("ic_dir").resizable().frame(width: 120, height: 120).scaledToFit()
+                            .overlay(Text(photoDir.name))
+                            .padding(.bottom, 8)
+                    })
+                }
             }
         }
     }
 
     func photosView() -> some View {
-        Form {
-            ScrollView(.horizontal) {
-                HStack(spacing: 16) {
-                    ForEach(model.value.dirs, id: \.self.id) { photoDir in
-                        Button(action: {
-                            photoList.onDirClicked(dirId: photoDir.id)
-                        }, label: {
-                            Image("ic_dir").resizable().frame(width: 120, height: 120).scaledToFit()
-                                    .overlay(Text(photoDir.name))
-                                    .padding(.bottom, 8)
-                        })
-                    }
+        ScrollView(.vertical) {
+            let columns: [GridItem] = [GridItem(.adaptive(minimum: 120))]
+            LazyVGrid(columns: columns) {
+                ForEach(model.value.photoImages, id: \.self.id) { photoImage in
+                    // try to do converting outside of scroll view to increase a performance
+                    let uiImage = (photoImage.image as KotlinByteArray).toUiImage() ?? UIImage()
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .clipped()
+                        .cornerRadius(4)
                 }
-                Spacer().frame(height: 4)
-            }
-            List(model.value.photoImages, id: \.self) { photoImage in
-                let uiImage = (photoImage.image as KotlinByteArray).toUiImage() ?? UIImage()
-                Image(uiImage: uiImage)
             }
         }
     }
